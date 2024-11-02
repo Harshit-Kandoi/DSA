7b5c2819cbc314fc51854169204deaa07c7a43d9
Harshit Kandoi <147999456+Harshit-Kandoi@users.noreply.github.com>
2024-11-02 13:01:49 +0530

    Add files via upload

---
+++ AdvancedPattern.java
@@ -0,0 +1,189 @@
+public class AdvancedPattern {
+    
+    // Prints a hollow rectangle of given rows and columns
+    public static void hollowRectangle(int totRows, int totCols) {
+        for (int i = 1; i <= totRows; i++) {
+            for (int j = 1; j <= totCols; j++) {
+                if (i == 1 || i == totRows || j == 1 || j == totCols) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+
+    // Prints an inverted rotated half-pyramid
+    public static void invertedRotatedHalfPyramid(int n) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+    // Prints an inverted half-pyramid with numbers
+    public static void invertedHalfPyramidWithNumbers(int n) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n - i + 1; j++) {
+                System.out.print(j + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    // Prints a Floyd's triangle
+    public static void floydTriangle(int n) {
+        int counter = 1;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(counter + " ");
+                counter++;
+            }
+            System.out.println();
+        }
+    }
+
+    // Prints a 0-1 triangle pattern
+    public static void zeroOneTriangle(int n) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print((i + j) % 2 == 0 ? "1" : "0");
+            }
+            System.out.println();
+        }
+    }
+
+    // Prints a butterfly pattern
+    public static void butterflyPattern(int n) {
+        // Upper half
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            for (int j = 1; j <= 2 * (n - i); j++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        // Lower half
+        for (int i = n; i >= 1; i--) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            for (int j = 1; j <= 2 * (n - i); j++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+    // Prints a solid rhombus pattern
+    public static void solidRhombus(int n) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= (n - i); j++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= n; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+    // Prints a hollow rhombus pattern
+    public static void hollowRhombus(int n) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= (n - i); j++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= n; j++) {
+                if (i == 1 || i == n || j == 1 || j == n) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+
+    // Prints a diamond pattern
+    public static void diamond(int n) {
+        // Upper half
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= (n - i); j++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= (2 * i) - 1; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        // Lower half
+        for (int i = n - 1; i >= 1; i--) {
+            for (int j = 1; j <= (n - i); j++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= (2 * i) - 1; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+    // Prints a number pyramid pattern
+    public static void numberPyramid(int n) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= (n - i); j++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= i; j++) {
+                System.out.print(i + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    // Prints a palindromic number pyramid pattern
+    public static void palindromicNumber(int n) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= (n - i); j++) {
+                System.out.print(" ");
+            }
+            for (int j = i; j >= 1; j--) {
+                System.out.print(j);
+            }
+            for (int j = 2; j <= i; j++) {
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        // Uncomment the method call for the pattern you want to display
+        // hollowRectangle(4, 5);
+        // invertedRotatedHalfPyramid(5);
+        // invertedHalfPyramidWithNumbers(5);
+        // floydTriangle(5);
+        // zeroOneTriangle(5);
+        // butterflyPattern(5);
+        // solidRhombus(5);
+        // hollowRhombus(5);
+        // diamond(4);
+        // numberPyramid(4);
+        palindromicNumber(4);
+    }
+}
